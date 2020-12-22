e1cf0648bebc2364da557e4ee129177189ea941a
adamdune <adam.ho.buss@gmail.com>
2020-12-22 20:50:46 +0800

    ref: day22 - cleaned, optimized slightly

--- 2020/day22/day22.py
+++ 2020/day22/day22.py
@@ -13,83 +13,60 @@ def get_puzzle_input(test_case=0):
         return tuple(parse_deck(deck) for deck in decks)
 
 
-def play_combat(deck1, deck2):
-    deck1 = deck1.copy()
-    deck2 = deck2.copy()
-
-    while len(deck1) != 0 and len(deck2) != 0:
-        card1 = deck1.popleft()
-        card2 = deck2.popleft()
-        if card1 > card2:
-            deck1.append(card1)
-            deck1.append(card2)
-        else:
-            deck2.append(card2)
-            deck2.append(card1)
-    return (deck1, deck2)
-
-
-def play_recurse_combat(deck1, deck2):
-    deck1 = deck1.copy()
-    deck2 = deck2.copy()
-
+def play_combat(deck1, deck2, part2=False):
     previous_rounds = set()
-
-    while len(deck1) != 0 and len(deck2) != 0:
-        round = (tuple(deck1), tuple(deck2))
-        if round in previous_rounds:
-            return (deck1, [])
-        else:
-            previous_rounds.add(round)
+    while deck1 and deck2:
+        if part2:
+            current_round = (tuple(deck1), tuple(deck2))
+            if current_round in previous_rounds:
+                return (deck1, [])
+            else:
+                previous_rounds.add(current_round)
 
         card1 = deck1.popleft()
         card2 = deck2.popleft()
 
-        if len(deck1) >= card1 and len(deck2) >= card2:
-            recurse_deck1, _recurse_deck2 = play_recurse_combat(
-                deque(list(deck1)[:card1]), deque(list(deck2)[:card2])
+        if part2 and len(deck1) >= card1 and len(deck2) >= card2:
+            recurse_deck1, _recurse_deck2 = play_combat(
+                deque(list(deck1)[:card1]),
+                deque(list(deck2)[:card2]),
+                part2,
             )
             if len(recurse_deck1) != 0:
-                deck1.append(card1)
-                deck1.append(card2)
+                deck1.extend([card1, card2])
             else:
-                deck2.append(card2)
-                deck2.append(card1)
+                deck2.extend([card2, card1])
+
         elif card1 > card2:
-            deck1.append(card1)
-            deck1.append(card2)
+            deck1.extend([card1, card2])
         else:
-            deck2.append(card2)
-            deck2.append(card1)
+            deck2.extend([card2, card1])
 
     return (deck1, deck2)
 
 
-def main(decks):
-    deck1, deck2 = play_combat(decks[0], decks[1])
-    re_deck1, re_deck2 = play_recurse_combat(decks[0], decks[1])
+def solution1(deck1, deck2):
+    deck1, deck2 = play_combat(deck1.copy(), deck2.copy())
+    # one of the deck is empty, so this doesn't matter
+    combined = deck1 + deck2
 
-    part1 = 0
-    for x in [deck1, deck2]:
-        if len(x) != 0:
-            for i, y in enumerate(x):
-                part1 += y * (len(x) - i)
-    part2 = 0
-    for x in [re_deck1, re_deck2]:
-        if len(x) != 0:
-            for i, y in enumerate(x):
-                part2 += y * (len(x) - i)
+    return sum([(len(combined) - i) * x for i, x in enumerate(combined)])
 
-    print("part1", part1)
-    print("part2", part2)
+
+def solution2(deck1, deck2):
+    deck1, deck2 = play_combat(deck1.copy(), deck2.copy(), part2=True)
+    combined = deck1 + deck2
+
+    return sum([(len(combined) - i) * x for i, x in enumerate(combined)])
 
 
 if __name__ == "__main__":
     from sys import argv
 
-    starttime = time()
-    puzzle_input = get_puzzle_input(
+    start_time = time()
+    deck1, deck2 = get_puzzle_input(
         argv[argv.index("--test") + 1] if "--test" in argv[1:] else 0
     )
-    main(puzzle_input)
-    print("%s seconds" % (time() - starttime))
+    print(solution1(deck1, deck2))
+    print(solution2(deck1, deck2))
+    print(f"Time: {'{0:.2f}'.format(time() - start_time)} seconds")

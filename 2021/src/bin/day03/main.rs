549a8cb7f84882e0d59bc1d20aedb4cf79240fb8
adamdune <adam.ho.buss@gmail.com>
2021-12-04 12:38:58 +0800

    2021 day03 cleaned

--- 2021/src/bin/day03/main.rs
+++ 2021/src/bin/day03/main.rs
@@ -7,62 +7,66 @@ fn parsed_input() -> Vec<Vec<char>> {
         .collect();
 }
 
-fn solution1() -> isize {
+fn string_binary_to_int(string_binary: &str) -> i32 {
+    i32::from_str_radix(string_binary, 2).unwrap()
+}
+
+fn solution1() -> i32 {
     let input = parsed_input();
 
     let mut count_of_1_bit: Vec<i32> = vec![0; input[0].len()];
-    let mut i = 0;
-    while i < input.len() {
-        let mut j = 0;
-        while j < input[i].len() {
-            if input[i][j] == '1' {
-                count_of_1_bit[j] += 1;
+    for bits in &input {
+        for (i, bit) in bits.iter().enumerate() {
+            if bit == &'1' {
+                count_of_1_bit[i] += 1;
             }
-            j += 1;
         }
-
-        i += 1;
     }
 
-    let mut k = 0;
     let mut gamma_binary = "".to_owned();
     let mut epsilon_binary = "".to_owned();
-    while k < count_of_1_bit.len() {
-        if count_of_1_bit[k] > input.len() as i32 / 2 {
+    for count_of_1_bit in count_of_1_bit {
+        let is_1_most_common = count_of_1_bit as f64 > input.len() as f64 / 2.0;
+        if is_1_most_common {
             gamma_binary.push_str("1");
             epsilon_binary.push_str("0");
         } else {
             gamma_binary.push_str("0");
             epsilon_binary.push_str("1");
         }
-        k += 1;
     }
 
-    return isize::from_str_radix(&gamma_binary, 2).unwrap()
-        * isize::from_str_radix(&epsilon_binary, 2).unwrap();
+    string_binary_to_int(&gamma_binary) * string_binary_to_int(&epsilon_binary)
 }
 
-fn solution2() -> isize {
-    let mut input1 = parsed_input();
+enum Rating {
+    OxygenGenerator,
+    Co2Scrubber,
+}
+
+fn generate_rating(rating: Rating) -> String {
+    let mut input = parsed_input();
+    let should_filter_by_1_bit = |count_of_1_bit: f64, input_length: f64| match rating {
+        Rating::OxygenGenerator => count_of_1_bit >= input_length / 2.0,
+        Rating::Co2Scrubber => count_of_1_bit < input_length / 2.0,
+    };
 
     let mut bit_position = 0;
-    while input1.len() > 1 {
+    while input.len() > 1 {
         let mut count_of_1_bit = 0;
-        let mut i = 0;
-        while i < input1.len() {
-            if input1[i][bit_position] == '1' {
+        for bytes in &input {
+            if bytes[bit_position] == '1' {
                 count_of_1_bit += 1;
             }
-            i += 1;
         }
 
-        if count_of_1_bit as f64 >= input1.len() as f64 / 2.0 {
-            input1 = input1
+        if should_filter_by_1_bit(count_of_1_bit as f64, input.len() as f64) {
+            input = input
                 .into_iter()
                 .filter(|x| x[bit_position] == '1')
                 .collect()
         } else {
-            input1 = input1
+            input = input
                 .into_iter()
                 .filter(|x| x[bit_position] == '0')
                 .collect()
@@ -70,36 +74,14 @@ fn solution2() -> isize {
         bit_position += 1;
     }
 
-    let mut input2 = parsed_input();
+    input[0].iter().collect::<String>()
+}
 
-    let mut bit_position = 0;
-    while input2.len() > 1 {
-        let mut count_of_1_bit = 0;
-        let mut i = 0;
-        while i < input2.len() {
-            if input2[i][bit_position] == '1' {
-                count_of_1_bit += 1;
-            }
-            i += 1;
-        }
+fn solution2() -> i32 {
+    let oxygen_generator_rating = generate_rating(Rating::OxygenGenerator);
+    let co2_scrubber_rating = generate_rating(Rating::Co2Scrubber);
 
-        if count_of_1_bit as f64 >= input2.len() as f64 / 2.0 {
-            input2 = input2
-                .into_iter()
-                .filter(|x| x[bit_position] == '0')
-                .collect()
-        } else {
-            input2 = input2
-                .into_iter()
-                .filter(|x| x[bit_position] == '1')
-                .collect()
-        }
-        bit_position += 1;
-    }
-    let bin1 = input1[0].iter().collect::<String>();
-    let bin2 = input2[0].iter().collect::<String>();
-
-    return isize::from_str_radix(&bin1, 2).unwrap() * isize::from_str_radix(&bin2, 2).unwrap();
+    string_binary_to_int(&oxygen_generator_rating) * string_binary_to_int(&co2_scrubber_rating)
 }
 
 fn main() {

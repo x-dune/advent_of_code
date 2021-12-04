dff3e84b49478f53f2e0981e02560dd5f881af86
adamdune <adam.ho.buss@gmail.com>
2021-12-04 16:32:43 +0800

    2021 day04 raw

---
+++ 2021/src/bin/day04/main.rs
@@ -0,0 +1,109 @@
+use aoc2021::util;
+
+type Board = Vec<Vec<i32>>;
+type Boards = Vec<Board>;
+
+fn get_drawn_numbers() -> Vec<i32> {
+    let input = util::input_as_string(include_str!("input.txt"));
+    let input_splitted = input.split("\n\n").collect::<Vec<&str>>();
+    let drawn_numbers = input_splitted[0]
+        .split(",")
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    drawn_numbers
+}
+
+fn get_boards() -> Boards {
+    let input = util::input_as_string(include_str!("input.txt"));
+    let boards = input
+        .split("\n\n")
+        .skip(1)
+        .map(|a| {
+            a.split("\n")
+                .map(|b| {
+                    b.split_whitespace()
+                        .map(|z| z.parse::<i32>().unwrap())
+                        .collect()
+                })
+                .collect()
+        })
+        .collect::<Boards>();
+
+    boards
+}
+
+fn is_board_a_winner(board: &Board) -> bool {
+    for row in board {
+        if row.iter().all(|x| *x == -1) {
+            return true;
+        }
+    }
+
+    let transposed = util::transpose(board.to_vec());
+    for row in transposed {
+        if row.iter().all(|x| *x == -1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn mark_board(board: &mut Board, number: i32) {
+    for row in board.iter_mut() {
+        for cell in row.iter_mut() {
+            if cell == &number {
+                *cell = -1;
+            }
+        }
+    }
+}
+
+fn sum_board(board: &Board) -> i32 {
+    let mut sum = 0;
+    for row in board {
+        for cell in row {
+            if *cell != -1 {
+                sum += cell;
+            }
+        }
+    }
+    sum
+}
+
+fn solution1() -> i32 {
+    let drawn_numbers = get_drawn_numbers();
+    let mut boards = get_boards();
+    for number in drawn_numbers {
+        for board in boards.iter_mut() {
+            mark_board(board, number);
+            if is_board_a_winner(&board) {
+                return sum_board(&board) * number;
+            }
+        }
+    }
+    return 0;
+}
+
+fn solution2() -> i32 {
+    let drawn_numbers = get_drawn_numbers();
+    let mut boards = get_boards();
+
+    let mut winner_indexes: Vec<usize> = vec![];
+    for number in drawn_numbers {
+        for (i, board) in boards.iter_mut().enumerate() {
+            if !winner_indexes.contains(&i) {
+                mark_board(board, number);
+                if is_board_a_winner(&board) {
+                    winner_indexes.push(i);
+                    println!("{}", sum_board(&board) * number);
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+fn main() {
+    println!("{} {}", solution1(), solution2())
+}
--- 2021/src/util.rs
+++ 2021/src/util.rs
@@ -1,3 +1,7 @@
+pub fn input_as_string(input: &str) -> &str {
+    return input.trim();
+}
+
 pub fn input_as_lines(input: &str) -> Vec<&str> {
     return input.trim().split("\n").collect::<Vec<&str>>();
 }
@@ -8,4 +12,18 @@ pub fn input_as_ints(input: &str) -> Vec<i32> {
         .split("\n")
         .map(|x| x.parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
+}
+
+pub fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    assert!(!v.is_empty());
+    let len = v[0].len();
+    let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
+    (0..len)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .map(|n| n.next().unwrap())
+                .collect::<Vec<T>>()
+        })
+        .collect()
 }

e16058d7efc747badb2f69b36cf3aa0e68e7bf23
adamdune <adam.ho.buss@gmail.com>
2021-12-07 23:06:46 +0800

    remove hashmap because it didn't save time

--- 2021/src/bin/day07/main.rs
+++ 2021/src/bin/day07/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, i32::MAX};
+use std::i32::MAX;
 
 use aoc2021::util;
 
@@ -13,20 +13,15 @@ fn main() {
     let min = input.iter().min().unwrap();
     let max = input.iter().max().unwrap();
 
-    let horizontal_pos_map = input.iter().fold(HashMap::new(), |mut acc, curr| {
-        *acc.entry(*curr).or_insert(0) += 1;
-        acc
-    });
-
     let mut answer1: i32 = MAX;
     let mut answer2: i32 = MAX;
 
     for new_pos in *min..=*max {
-        let (possible_answer1, possible_answer2) = horizontal_pos_map
+        let (possible_answer1, possible_answer2) = input
             .iter()
-            .map(|(horizontal_pos, count)| {
+            .map(|horizontal_pos| {
                 let diff = (horizontal_pos - new_pos).abs();
-                (diff * count, triangle_sum(diff) * count)
+                (diff, triangle_sum(diff))
             })
             .fold((0, 0), |acc, curr| (acc.0 + curr.0, acc.1 + curr.1));
 

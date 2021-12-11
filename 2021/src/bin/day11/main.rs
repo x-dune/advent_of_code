74a660dd60b256595540e0d63c6178b709728b01
adamdune <adam.ho.buss@gmail.com>
2021-12-11 15:01:19 +0800

    2021 day11 cleaned

--- 2021/src/bin/day11/main.rs
+++ 2021/src/bin/day11/main.rs
@@ -2,34 +2,29 @@ use std::collections::{BTreeMap, HashSet, VecDeque};
 
 use aoc2021::util;
 
-type Grid = BTreeMap<(i32, i32), u32>;
+type GridMap = BTreeMap<(i32, i32), u32>;
 
-fn step(grid: Grid) -> (Grid, usize) {
-    let mut next_grid = grid
+fn step(grid_map: GridMap) -> (GridMap, usize) {
+    let mut q = VecDeque::new();
+    let mut next_grid_map = grid_map
         .iter()
-        .map(|(coord, energy)| (*coord, *energy + 1))
-        .collect::<Grid>();
-    let mut to_flash = next_grid
-        .iter()
-        .filter_map(|(coord, energy)| {
-            if energy > &&9 {
-                return Some(*coord);
+        .map(|(coord, energy)| {
+            let next_energy = *energy + 1;
+            if next_energy > 9 {
+                q.push_back(*coord);
             }
-            return None;
+            (*coord, *energy + 1)
         })
-        .fold(VecDeque::new(), |mut acc, curr| {
-            acc.push_back(curr);
-            acc
-        });
-    let mut has_flashed = HashSet::new();
+        .collect::<GridMap>();
+    let mut flashed = HashSet::new();
 
-    while to_flash.len() > 0 {
-        let current @ (x, y) = to_flash.pop_front().unwrap();
-        if !has_flashed.insert(current) {
+    while q.len() > 0 {
+        let current @ (x, y) = q.pop_front().unwrap();
+        if !flashed.insert(current) {
             continue;
         }
 
-        *next_grid.get_mut(&current).unwrap() = 0;
+        *next_grid_map.get_mut(&current).unwrap() = 0;
 
         let neighbours = [
             (x + 1, y),
@@ -43,24 +38,24 @@ fn step(grid: Grid) -> (Grid, usize) {
         ];
 
         for d in neighbours {
-            if has_flashed.contains(&d) {
+            if flashed.contains(&d) {
                 continue;
             }
 
-            if let Some(energy) = next_grid.get_mut(&d) {
+            if let Some(energy) = next_grid_map.get_mut(&d) {
                 *energy += 1;
                 if *energy > 9 {
-                    to_flash.push_back(d)
+                    q.push_back(d)
                 }
             }
         }
     }
 
-    (next_grid, has_flashed.len())
+    (next_grid_map, flashed.len())
 }
 
 fn main() {
-    let input = util::input_as_lines(include_str!("input.txt"))
+    let mut grid_map = util::input_as_lines(include_str!("input.txt"))
         .iter()
         .enumerate()
         .fold(BTreeMap::new(), |mut acc, (y, row)| {
@@ -70,27 +65,26 @@ fn main() {
             acc
         });
 
-    let mut input_clone = input.clone();
     let mut total_flashes = 0;
     let mut has_all_flashed = false;
     for i in 1.. {
-        let (next_grid, flashes) = step(input_clone);
+        let (next_grid_map, flashes) = step(grid_map);
         total_flashes += flashes;
-        input_clone = next_grid;
+        grid_map = next_grid_map;
 
-        if flashes == 100 {
+        if flashes == grid_map.len() {
             has_all_flashed = true;
         }
 
         if i == 100 {
-            println!("{}", total_flashes);
-            if has_all_flashed {
-                break;
-            }
+            println!("part 1: {}", total_flashes);
         }
 
-        if has_all_flashed && i > 100 {
-            println!("{}", i);
+        if has_all_flashed {
+            println!("part 2: {}", i);
+        }
+
+        if i >= 100 && has_all_flashed {
             break;
         }
     }

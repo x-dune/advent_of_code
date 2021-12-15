86f9c58866144f4dd7fb02df5a5f07ab26d01944
adamdune <adam.ho.buss@gmail.com>
2021-12-16 00:46:40 +0800

    ref(2021_15) cleaned

--- 2021/src/bin/day15/main.rs
+++ 2021/src/bin/day15/main.rs
@@ -3,117 +3,85 @@ use std::{
     collections::{BTreeMap, BinaryHeap},
 };
 
-fn get_risk_level(grid: &BTreeMap<(i32, i32), i32>) -> Option<i32> {
+fn dijkstra(grid: &BTreeMap<(i32, i32), i32>) -> i32 {
     let start = (0, 0);
-    let end = grid.iter().fold((0, 0), |acc, (curr, _)| {
-        (max(acc.0, curr.0 as i32), max(acc.1, curr.1 as i32))
+    let end = grid.iter().fold((0, 0), |acc, (cord, _)| {
+        (max(acc.0, cord.0), max(acc.1, cord.1))
     });
 
     let mut dist = BTreeMap::new();
     let mut q = BinaryHeap::new();
-    let mut prev = BTreeMap::new();
-    q.push(Reverse((0, start)));
-    dist.insert(start, 0);
 
-    while q.len() > 0 {
-        let Reverse((cost, pos)) = q.pop().unwrap();
+    dist.insert(start, 0);
+    q.push(Reverse((0, start)));
+
+    while let Some(Reverse((cost, pos @ (x, y)))) = q.pop() {
         if pos == end {
-            return Some(cost);
+            return cost;
         }
 
         if cost > *dist.get(&pos).unwrap_or(&i32::MAX) {
             continue;
         }
 
-        [
-            (pos.0 + 1, pos.1),
-            (pos.0 - 1, pos.1),
-            (pos.0, pos.1 + 1),
-            (pos.0, pos.1 - 1),
-        ]
-        .iter()
-        .filter_map(|n| grid.get(&n).map(|n_cost| (n_cost, n)))
-        .for_each(|(n_cost, &n)| {
-            let next_cost = cost + n_cost;
-            if next_cost < *dist.get(&n).unwrap_or(&i32::MAX) {
-                dist.insert(n, next_cost);
-                q.push(Reverse((next_cost, n)));
-                prev.insert(n, pos);
-            }
-        })
+        [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+            .iter()
+            .for_each(|&n_pos| {
+                if let Some(d_cost) = grid.get(&n_pos) {
+                    let next_cost = d_cost + cost;
+                    if next_cost < *dist.get(&n_pos).unwrap_or(&i32::MAX) {
+                        dist.insert(n_pos, next_cost);
+                        q.push(Reverse((next_cost, n_pos)))
+                    }
+                }
+            });
     }
-
-    return None;
-}
-
-fn insert_expanded(
-    grid: &mut BTreeMap<(i32, i32), i32>,
-    pos: (i32, i32),
-    risk: i32,
-    original_grid_size: i32,
-) {
-    for i in 0..5 {
-        let left_most_risk = risk + i;
-        [
-            left_most_risk,
-            left_most_risk + 1,
-            left_most_risk + 2,
-            left_most_risk + 3,
-            left_most_risk + 4,
-        ]
-        .iter()
-        .map(|&r| if r > 9 { r % 9 } else { r })
-        .enumerate()
-        .for_each(|(j, r)| {
-            grid.insert(
-                (
-                    pos.0 + (original_grid_size * j as i32),
-                    pos.1 + (original_grid_size * i),
-                ),
-                r,
-            );
-        })
-    }
+    unreachable!();
 }
 
 fn main() {
-    let grid = include_str!("input.txt")
+    let input = include_str!("input.txt");
+    let grid_horizontal_size = input.lines().nth(0).unwrap().len() as i32;
+    let grid_vertical_size = input.lines().count() as i32;
+    let grid = input
         .lines()
-        .map(|s| {
-            s.chars()
-                .map(|c| c.to_digit(10).unwrap() as i32)
+        .enumerate()
+        .flat_map(|(y, l)| {
+            l.chars()
+                .enumerate()
+                .map(|(x, c)| ((x as i32, y as i32), c.to_digit(10).unwrap() as i32))
                 .collect::<Vec<_>>()
         })
-        .enumerate()
-        .fold(BTreeMap::new(), |mut acc, (y, v)| {
-            for (x, risk_level) in v.into_iter().enumerate() {
-                acc.insert((x as i32, y as i32), risk_level);
-            }
-            acc
-        });
+        .collect::<BTreeMap<_, _>>();
 
-    let original_grid_size = (grid.len() as f64).sqrt() as i32;
+    let answer1 = dijkstra(&grid);
 
-    let grid_expanded = include_str!("input.txt")
-        .lines()
-        .map(|s| {
-            s.chars()
-                .map(|c| c.to_digit(10).unwrap() as i32)
-                .collect::<Vec<_>>()
+    println!("{}", answer1);
+
+    let expanded_grid = &grid
+        .iter()
+        .flat_map(|(&pos, &risk)| {
+            (0..5).flat_map(move |i| {
+                (0..5).map(move |j| {
+                    let expanded_risk = risk + i + j;
+                    let normalized_risk = if expanded_risk > 9 {
+                        expanded_risk % 9
+                    } else {
+                        expanded_risk
+                    };
+                    (
+                        (
+                            pos.0 + i * grid_horizontal_size,
+                            pos.1 + j * grid_vertical_size,
+                        ),
+                        normalized_risk,
+                    )
+                })
+            })
         })
-        .enumerate()
-        .fold(BTreeMap::new(), |mut acc, (y, v)| {
-            for (x, risk_level) in v.into_iter().enumerate() {
-                insert_expanded(
-                    &mut acc,
-                    (x as i32, y as i32),
-                    risk_level,
-                    original_grid_size,
-                );
-            }
-            acc
-        });
+        .collect::<BTreeMap<_, _>>();
 
-    println!("{:?}", get_risk_level(&grid));
-    println!("{:?}", get_risk_level(&grid_expanded));
+    let answer2 = dijkstra(&expanded_grid);
+
+    println!("{}", answer2);
 }

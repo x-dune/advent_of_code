041af8fe1043fb0bc2e51d16bfbcb7029c181a1b
adamdune <adam.ho.buss@gmail.com>
2021-12-18 00:21:52 +0800

    ref(2021_17) cleaned

--- 2021/src/bin/day17/main.rs
+++ 2021/src/bin/day17/main.rs
@@ -1,8 +1,7 @@
-use std::cmp::max;
+use std::ops::RangeInclusive;
 
-fn parse_input() -> ((i32, i32), (i32, i32)) {
+fn parse_input() -> (RangeInclusive<i32>, RangeInclusive<i32>) {
     let (_, x_y) = include_str!("input.txt").trim().split_once(": ").unwrap();
-
     let ranges = x_y
         .split(", ")
         .map(|s| {
@@ -15,50 +14,41 @@ fn parse_input() -> ((i32, i32), (i32, i32)) {
         })
         .collect::<Vec<_>>();
 
-    // x range, y range
-    (ranges[0], ranges[1])
+    // range_x, range_y
+    (ranges[0].0..=ranges[0].1, ranges[1].0..=ranges[1].1)
 }
 
-fn step((x, y): (i32, i32), (dx, dy): (i32, i32)) -> ((i32, i32), (i32, i32)) {
-    let next_pos = (x + dx, y + dy);
-
-    let next_dx = if dx > 0 {
-        dx - 1
-    } else if dx < 0 {
-        dx + 1
-    } else {
-        0
-    };
-    let next_dy = dy - 1;
-
-    (next_pos, (next_dx, next_dy))
-}
-
-fn try_velocity(start_vel: (i32, i32), (goal_x, goal_y): ((i32, i32), (i32, i32))) -> Option<i32> {
-    let (range_x, range_y) = (goal_x.0..=goal_x.1, goal_y.0..=goal_y.1);
+fn try_velocity(
+    start_vel: (i32, i32),
+    range_x: &RangeInclusive<i32>,
+    range_y: &RangeInclusive<i32>,
+) -> Option<i32> {
     let mut pos = (0, 0);
-    let mut vel = start_vel;
+    let (mut dx, mut dy) = start_vel;
     let mut highest_pos_y = 0;
 
-    while !(range_x.contains(&pos.0) && range_y.contains(&pos.1)) {
-        let (next_pos, next_vel) = step(pos, vel);
-        pos = next_pos;
-        vel = next_vel;
-        highest_pos_y = max(highest_pos_y, pos.1);
+    loop {
+        pos = (pos.0 + dx, pos.1 + dy);
+        dx -= dx.signum();
+        dy -= 1;
 
-        if (vel.0 == 0 && !range_x.contains(&pos.0)) || (vel.1 < 0 && pos.1 < goal_y.0) {
+        if pos.1 > highest_pos_y {
+            highest_pos_y = pos.1;
+        }
+
+        if range_x.contains(&pos.0) && range_y.contains(&pos.1) {
+            return Some(highest_pos_y);
+        } else if (dx == 0 && !range_x.contains(&pos.0)) || (dy < 0 && pos.1 < *range_y.start()) {
             return None;
         }
     }
-
-    return Some(highest_pos_y);
 }
 
 fn main() {
-    let goal = parse_input();
-
-    let max_heights = (-250..=250)
-        .flat_map(|x| (-250..=250).filter_map(move |y| try_velocity((x, y), goal)))
+    let (range_x, range_y) = parse_input();
+    let max_heights = (0..=*range_x.end())
+        .flat_map(|dx| (*range_y.start()..=1000).map(move |dy| (dx, dy)))
+        .filter_map(|v| try_velocity(v, &range_x, &range_y))
         .collect::<Vec<_>>();
 
     let answer1 = max_heights.iter().max().unwrap();

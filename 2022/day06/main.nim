f049fc4c766bc1e21b983bfeced7fdb642861757
x-dune <39018755+x-dune@users.noreply.github.com>
2022-12-06 20:57:09 +0800

    (2022/06) oh right, sets exists

--- 2022/day06/main.nim
+++ 2022/day06/main.nim
@@ -1,23 +1,18 @@
 import std/os
+import std/sets
 import std/strutils
 
 let input = readFile(currentSourcePath.parentDir & "/input.txt").strip
 
-for i, _ in input:
-  let chars = input[i..i+3]
-  if chars.rfind(input[i]) == 0 and chars.rfind(input[i + 1]) == 1 and
-      chars.rfind(input[i + 2]) == 2 and chars.rfind(input[i + 3]) == 3:
-    echo i + 4
-    break
+var answer1 = 0
+var answer2 = 0
+var i = 0
 
-for i, _ in input:
-  let chars = input[i..i+13]
-  if chars.rfind(input[i]) == 0 and chars.rfind(input[i + 1]) == 1 and
-      chars.rfind(input[i + 2]) == 2 and chars.rfind(input[i + 3]) == 3 and
-      chars.rfind(input[i + 4]) == 4 and chars.rfind(input[i + 5]) == 5 and
-      chars.rfind(input[i + 6]) == 6 and chars.rfind(input[i + 7]) == 7 and
-      chars.rfind(input[i + 8]) == 8 and chars.rfind(input[i + 9]) == 9 and
-      chars.rfind(input[i + 10]) == 10 and chars.rfind(input[i + 11]) == 11 and
-      chars.rfind(input[i + 12]) == 12 and chars.rfind(input[i + 13]) == 13:
-    echo i + 14
-    break
+while answer1 == 0 or answer2 == 0:
+  if answer1 == 0 and len(toHashSet(input[i..i+3])) == 4:
+    answer1 = i + 4
+  if answer2 == 0 and len(toHashSet(input[i..i+13])) == 14:
+    answer2 = i + 14
+  i += 1
+
+echo answer1, '\n', answer2

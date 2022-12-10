bacb75077314e62f0fd1432829b05cb1345595b4
x-dune <39018755+x-dune@users.noreply.github.com>
2022-12-10 14:17:16 +0800

    (2022/10) add solution

---
+++ 2022/day10/main.nim
@@ -0,0 +1,37 @@
+import std/sequtils
+import std/strutils
+import std/sugar
+
+let input = readAll(stdin).strip.splitLines
+
+let temp = collect:
+  for _, instruction in input:
+    if instruction == "noop": @[instruction]
+    # prepend addx instruction with noop to simulate addx taking two cycles
+    else: @["noop", instruction]
+# flatten seq[seq[string]] to seq[string]
+let prependedInput = temp.concat.concat
+
+var x = 1
+var xs = newSeq[int]()
+var answer2 = collect(for _ in 0..5: "........................................")
+for i, instruction in prependedInput:
+  let cycle = i + 1
+
+  if cycle in [20, 60, 100, 140, 180, 220]:
+    xs.add(cycle * x)
+
+  let spritePosition = [x-1, x, x+1]
+  let crtY = ((cycle-1) div 40)
+  let crtX = (cycle-1) mod 40
+  if crtX in spritePosition:
+    answer2[crtY][crtX] = '#'
+
+  if instruction != "noop":
+    x += instruction.splitWhitespace[1].parseInt
+
+let answer1 = xs.foldl(a+b)
+echo answer1
+# Print answer 2
+for _, line in answer2:
+  echo line

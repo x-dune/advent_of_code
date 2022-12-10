6c3582784d33479b49d02e55c2a3fe9763d91958
x-dune <39018755+x-dune@users.noreply.github.com>
2022-12-10 14:30:17 +0800

    style: +readable

--- 2022/day10/main.nim
+++ 2022/day10/main.nim
@@ -6,19 +6,19 @@ let input = readAll(stdin).strip.splitLines
 
 let temp = collect:
   for _, instruction in input:
-    if instruction == "noop": @[instruction]
     # prepend addx instruction with noop to simulate addx taking two cycles
-    else: @["noop", instruction]
+    if instruction.startsWith("addx"): @["noop", instruction]
+    else: @[instruction]
 # flatten seq[seq[string]] to seq[string]
 let prependedInput = temp.concat.concat
 
 var x = 1
-var xs = newSeq[int]()
+var answer1 = 0
 var answer2 = collect(for _ in 0..5: "........................................")
 for i, instruction in prependedInput:
   let cycle = i + 1
   if cycle in [20, 60, 100, 140, 180, 220]:
-    xs.add(cycle * x)
+    answer1 += cycle * x
 
   let spritePositions = [x-1, x, x+1]
   let crtY = i div 40
@@ -26,10 +26,9 @@ for i, instruction in prependedInput:
   if crtX in spritePositions:
     answer2[crtY][crtX] = '#'
 
-  if instruction != "noop":
+  if instruction.startsWith("addx"):
     x += instruction.splitWhitespace[1].parseInt
 
-let answer1 = xs.foldl(a+b)
 echo answer1
 # Print answer 2
 for _, line in answer2:

b695d528cc1cb4bdbc121247a50161f80128defa
x-dune <39018755+x-dune@users.noreply.github.com>
2023-01-03 23:17:37 +0800

    optimize day16 by filtering dupes states

--- 2022/day16/main.nim
+++ 2022/day16/main.nim
@@ -4,6 +4,7 @@ import std/nre
 import std/sequtils
 import std/strutils
 import std/tables
+import std/sets
 
 type Vertex = (seq[string], int)
 # minute, totalPressure, valve, opened
@@ -17,6 +18,7 @@ proc bfs(vertices: Table[string, Vertex]): int =
   var q = [start].toDeque
   var depth = 1
   var maxPressure = 0
+  var seen = [start].toHashSet
   while q.len > 0:
     if q.peekFirst[0] > depth:
       depth = q.peekFirst[0]
@@ -29,14 +31,19 @@ proc bfs(vertices: Table[string, Vertex]): int =
         maxPressure = totalPressure
       continue
 
+    var nextStates: seq[State]
     let (neighbours, flowRate) = vertices[valve]
     if flowRate > 0 and valve notin opened:
-      var nextOpened = opened
-      nextOpened.add(valve)
-      q.addLast((minute+1, totalPressure+flowRate*(30-minute), valve, nextOpened))
+      nextStates.add((minute+1, totalPressure+flowRate*(30-minute), valve, opened.concat(@[valve])))
 
     for n in neighbours:
-      q.addLast((minute+1, totalPressure, n, opened))
+      nextStates.add((minute+1, totalPressure, n, opened))
+
+    for nextState in nextStates:
+      if nextState notin seen:
+        seen.incl(nextState)
+        q.addLast(nextState)
+
   return maxPressure
 
 proc bfs2(vertices: Table[string, Vertex]): int =
@@ -44,6 +51,7 @@ proc bfs2(vertices: Table[string, Vertex]): int =
   var q = [start].toDeque
   var depth = 1
   var maxPressure = 0
+  var seen = [start].toHashSet
   while q.len > 0:
     if q.peekFirst[0] > depth:
       depth = q.peekFirst[0]
@@ -59,23 +67,26 @@ proc bfs2(vertices: Table[string, Vertex]): int =
     let (neighbours1, flowRate1) = vertices[valves[0]]
     var nextQ: seq[State2]
     if flowRate1 > 0 and valves[0] notin opened:
-      var nextOpened = opened
-      nextOpened.add(valves[0])
-      nextQ.add((minute+1, totalPressure+flowRate1*(26-minute), valves, nextOpened))
+      nextQ.add((minute+1, totalPressure+flowRate1*(26-minute), valves, opened.concat(@[valves[0]])))
 
     for n in neighbours1:
       nextQ.add((minute+1, totalPressure, [n, valves[1]], opened))
 
+    var nextStates: seq[State2]
     let (neighbours2, flowRate2) = vertices[valves[1]]
     for item in nextQ:
       let (minute1, totalPressure1, valves1, opened1) = item
       if flowRate2 > 0 and valves[1] notin opened1:
-        var nextOpened = opened1
-        nextOpened.add(valves[1])
-        q.addLast((minute1, totalPressure1+flowRate2*(26-minute), valves1, nextOpened))
+        nextStates.add((minute1, totalPressure1+flowRate2*(26-minute), valves1, opened1.concat(@[valves[1]])))
 
       for n in neighbours2:
-        q.addLast((minute1, totalPressure1, [valves1[0], n], opened1))
+        nextStates.add((minute1, totalPressure1, [valves1[0], n], opened1))
+
+      for nextState in nextStates:
+        if nextState notin seen:
+          seen.incl(nextState)
+          q.addLast(nextState)
+
   return maxPressure
 
 proc solve*(input: string): (int, int) =

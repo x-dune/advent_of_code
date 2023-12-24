f8673b5b04c64f0d1e42f465e3e285bdc793338c
x-dune <39018755+x-dune@users.noreply.github.com>
2023-12-24 23:13:44 +0800

    just use z3

--- 2023_temp/src/bin/day24/main.rs
+++ 2023_temp/src/bin/day24/main.rs
@@ -1,5 +1,7 @@
 use std::io::{self, BufRead};
 
+use z3::ast::Ast;
+
 type Vec3 = (i64, i64, i64);
 
 fn parse_input(input: &[String]) -> Vec<(Vec3, Vec3)> {
@@ -85,6 +87,41 @@ fn main() {
 
     let hailstones = parse_input(&input);
     let answer1 = get_intersections(&hailstones, (200000000000000., 400000000000000.));
-
     println!("{}", answer1);
+
+    let cfg = z3::Config::new();
+    let ctx = z3::Context::new(&cfg);
+    let solver = z3::Solver::new(&ctx);
+
+    let x = z3::ast::Int::new_const(&ctx, "x");
+    let y = z3::ast::Int::new_const(&ctx, "y");
+    let z = z3::ast::Int::new_const(&ctx, "z");
+    let vx = z3::ast::Int::new_const(&ctx, "vx");
+    let vy = z3::ast::Int::new_const(&ctx, "vy");
+    let vz = z3::ast::Int::new_const(&ctx, "vz");
+
+    for (i, hs) in hailstones.iter().take(3).enumerate() {
+        let a = z3::ast::Int::from_i64(&ctx, hs.0 .0);
+        let va = z3::ast::Int::from_i64(&ctx, hs.1 .0);
+        let b = z3::ast::Int::from_i64(&ctx, hs.0 .1);
+        let vb = z3::ast::Int::from_i64(&ctx, hs.1 .1);
+        let c = z3::ast::Int::from_i64(&ctx, hs.0 .2);
+        let vc = z3::ast::Int::from_i64(&ctx, hs.1 .2);
+
+        let t = z3::ast::Int::new_const(&ctx, format!("t{i}"));
+        solver.assert(&t.gt(&z3::ast::Int::from_i64(&ctx, 0)));
+        solver.assert(&(x.clone() + vx.clone() * t.clone())._eq(&(a + va * t.clone())));
+        solver.assert(&(y.clone() + vy.clone() * t.clone())._eq(&(b + vb * t.clone())));
+        solver.assert(&(z.clone() + vz.clone() * t.clone())._eq(&(c + vc * t.clone())));
+    }
+
+    if solver.check() == z3::SatResult::Sat {
+        if let Some(m) = solver.get_model() {
+            println!("{}", m.eval(&(x + y + z), true).unwrap());
+        } else {
+            println!("Failed to get answer 2");
+        }
+    } else {
+        println!("Failed to get answer 2");
+    }
 }

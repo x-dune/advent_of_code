ba265157847822e86b906b5fd23dc7b87f0b550b
x-dune <39018755+x-dune@users.noreply.github.com>
2025-05-30 23:14:57 +0800

    2024 day 08 part 2

--- 2024/src/day08/main.go
+++ 2024/src/day08/main.go
@@ -3,11 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	// "math"
 	"os"
-	// "slices"
-	// "strconv"
-	// "strings"
 )
 
 type Pos = [2]int
@@ -28,42 +24,67 @@ func main() {
 		}
 	}
 
-	set := make(map[Pos]bool)
+	set1 := make(map[Pos]bool)
+	set2 := make(map[Pos]bool)
 	for _, v := range pos {
 		for i, a := range v {
 			for _, b := range v[i+1:] {
-				for _, n := range getAntinodes(a, b, len(grid), len(grid[0])) {
-					set[n] = true
+				for _, n := range getAntinodes1(a, b, len(grid), len(grid[0])) {
+					set1[n] = true
+				}
+				for _, n := range getAntinodes2(a, b, len(grid), len(grid[0])) {
+					set2[n] = true
 				}
 			}
-			// fmt.Println(string(k), a, i)
 		}
-
-		// fmt.Println(k, v)
 	}
 
-	// printGrid(grid)
-	fmt.Println(len(set))
+	fmt.Println(len(set1))
+	fmt.Println(len(set2))
 }
 
-func printGrid(grid [][]rune) {
-	for _, col := range grid {
-		for _, cell := range col {
-			fmt.Print(string(cell))
-		}
-		fmt.Print("\n")
-	}
+func isInBounds(pos Pos, maxY, maxX int) bool {
+	return pos[0] >= 0 && pos[0] < maxY && pos[1] >= 0 && pos[1] < maxX
 }
 
-func getAntinodes(a, b Pos, maxY, maxX int) []Pos {
-	var antinodes []Pos
+func getAntinodes1(a, b Pos, maxY, maxX int) []Pos {
+	antinodes := []Pos{}
 	d := Pos{b[0] - a[0], b[1] - a[1]}
 	antiA := Pos{a[0] - d[0], a[1] - d[1]}
 	antiB := Pos{b[0] + d[0], b[1] + d[1]}
 	for _, pos := range []Pos{antiA, antiB} {
-		if pos[0] >= 0 && pos[0] < maxY && pos[1] >= 0 && pos[1] < maxX {
+		if isInBounds(pos, maxY, maxX) {
 			antinodes = append(antinodes, pos)
 		}
 	}
+	return antinodes
+}
+
+func getAntinodes2(a, b Pos, maxY, maxX int) []Pos {
+	// each antena that has a counterpart has an antinode on it
+	antinodes := []Pos{a, b}
+	d := Pos{b[0] - a[0], b[1] - a[1]}
+	i := 1
+	for {
+		antiA := Pos{a[0] - d[0]*i, a[1] - d[1]*i}
+		if isInBounds(antiA, maxY, maxX) {
+			antinodes = append(antinodes, antiA)
+		} else {
+			break
+		}
+		i++
+	}
+
+	i = 0
+	for {
+		antiB := Pos{b[0] + d[0]*i, b[1] + d[1]*i}
+		if isInBounds(antiB, maxY, maxX) {
+			antinodes = append(antinodes, antiB)
+		} else {
+			break
+		}
+		i++
+	}
+
 	return antinodes
 }
